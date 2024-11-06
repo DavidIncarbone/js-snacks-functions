0383b3d5be694caca21ab702f7fced7cc3ad6f55
DavidIncarbone <david.incarbone@gmail.com>
2024-11-06 16:09:38 +0100

    ex 4 done

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -13,28 +13,34 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
-function numberVowelsExtractor(text) {
-    const vowelArray = ["a", "e", "i", "o", "u"];
+// function numberVowelsExtractor(text) {
+//     const vowelArray = ["a", "e", "i", "o", "u"];
 
-    const newArray = [];
-    for (let iv = 0; iv < vowelArray.length; iv++) {
-        for (let i = 0; i < text.length; i++) {
-            if (text[i] === vowelArray[iv]) {
-                newArray.push(text[i]);
-            }
+//     const newArray = [];
+//     for (let iv = 0; iv < vowelArray.length; iv++) {
+//         for (let i = 0; i < text.length; i++) {
+//             if (text[i] === vowelArray[iv]) {
+//                 newArray.push(text[i]);
+//             }
 
-        }
-    }
-    return newArray;
-}
+//         }
+//     }
+//     return newArray;
+// }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-const vowelsTextArray = numberVowelsExtractor(word);
-console.log(vowelsTextArray);
+// const vowelsTextArray = numberVowelsExtractor(word);
+// console.log(vowelsTextArray);
 
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
 
 // arrow function
+
+const vowelArray = ["a", "e", "i", "o", "u"];
+const vowelsExtracted = [];
+
+word.filter((letter) => vowelsExtracted.push(word(letter) === vowelArray[i]));
+console.log(vowelsExtracted);
--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,3 +1,6 @@
+"use strict";
+console.clear();
+
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
 const name = 'Mario';
@@ -5,9 +8,17 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+function helloPeople(name) {
+
+    const helloInsertedName = `Ciao ${name}!`
+    return helloInsertedName
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const helloInsertedName = helloPeople(name);
+console.log(helloInsertedName);
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario

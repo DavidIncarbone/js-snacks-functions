e62e3746d55cdfb14661963a8058a333fa84a99f
DavidIncarbone <david.incarbone@gmail.com>
2024-11-06 16:33:16 +0100

    ex.bonus done

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -1,3 +1,6 @@
+"use strict";
+console.clear();
+
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito. 
 Il saluto deve essere :
 buongiorno se è mattina (fino alle 13), 
@@ -6,13 +9,39 @@ e buonasera se è sera (oltre le 17)
 */
 
 const name = 'Mario';
+const now = new Date();
+console.log(now.getHours());
 
 
 // Dichiara la funzione qui.
 
+function helloName(name) {
+
+    let helloInsertedName = `Ciao ${name}!`
+    if (now.getHours() >= 5 && now.getHours() <= 13) {
+
+        helloInsertedName = `Buongiorno ${name}!`
+    } else if (now.getHours() > 13 && now.getHours() <= 17) {
+
+        helloInsertedName = `Buonpomeriggio ${name}!`
+    } else if (now.getHours() > 17 && now.getHours() <= 21) {
+
+        helloInsertedName = `Buonasera ${name}!`
+    } else {
+
+        helloInsertedName = `Buonanotte ${name}!`
+    }
+
+    return helloInsertedName
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
+
+const helloInsertedName = helloName(name);
+console.log(helloInsertedName);
 
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
+

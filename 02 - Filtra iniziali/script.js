ee31b56b9e9636fd59d521949e8f192a334243c4
DavidIncarbone <david.incarbone@gmail.com>
2024-11-06 14:13:28 +0100

    done ex. 2

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -1,3 +1,6 @@
+"use strict";
+console.clear();
+
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
@@ -5,9 +8,31 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+/**
+ * 
+ * @param {array} myArray 
+ * @param {string} text 
+ * @returns 
+ * @abstract Mi ritorna un array contente solo i nomi che iniziano con una data lettera
+ */
+function stringArray(myArray, text) {
+
+    const newArray = [];
+
+    for (let i = 0; i < myArray.length; i++) {
+
+        if (myArray[i][0] === text) {
+
+            newArray.push(myArray[i]);
+        }
+    }
+    return newArray;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const choicedArray = stringArray(names, "A");
+console.log(choicedArray);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
